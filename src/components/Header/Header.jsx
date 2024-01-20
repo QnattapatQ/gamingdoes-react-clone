a441ba39385afa6fc5c56aa5d7585b2eed812d02
Nattapat_BookZ <nattapatbook2078@gmail.com>
2024-01-21 00:56:16 +0700

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,52 @@
 import React from 'react'
-import TopBannerAds from './TopBanner'
+import { useEffect, useState } from 'react'
+import Logo from '../../assets/images/logo_main.svg';
 
 const Header = () => {
+
+    const [stickyNav, setStickyNav] = useState(false);
+
+    const [scrollDetail, setScrollDetail] = useState({
+        scrollDetail: window.scrollY
+    })
+
+    const resetScroll = () => {
+        setScrollDetail({
+            scrollDetail: window.scrollY
+        });
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', resetScroll);
+
+        if(scrollDetail.scrollDetail >= 235) {
+            setStickyNav(true);
+        } else {
+            setStickyNav(false);
+        }
+
+    },[scrollDetail])
+
     return (
-        <div className='max-w-[1332px] w-full mx-auto'>
-            <div className='bg-red-500 p-2 text-2xl sticky top-0 left-0 right-0 w-full'>GamingDose</div>
+        <div className='p-4 w-full'>
+            <div className={`${stickyNav ? 'fixed top-0 left-0 right-0' : ''} max-w-[1332px] w-full mx-auto`}>
+                <div className='bg-red-500 p-2 text-2xl w-full flex items-center justify-between'>
+                    <div>
+                        <img src={Logo} alt="" />
+                    </div>
+                    <div>
+                        <ul className='px-4'>
+                            <li className='flex gap-8'>
+                                <a className='uppercase text-white' href="#">Latest</a>
+                                <a className='uppercase text-white' href="#">News</a>
+                                <a className='uppercase text-white' href="#">Read</a>
+                                <a className='uppercase text-white' href="#">Watch</a>
+                                <a className='uppercase text-white' href="#">About</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
