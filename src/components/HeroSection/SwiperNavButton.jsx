113f4d524f4972b99a3baafc6b873a46b00c6f67
Nattapat_BookZ <nattapatbook2078@gmail.com>
2024-02-26 00:15:07 +0700

    update

--- src/components/HeroSection/SwiperNavButton.jsx
+++ src/components/HeroSection/SwiperNavButton.jsx
@@ -8,7 +8,7 @@ const SwiperNavButton = ({ currentIndex }) => {
     const swiper = useSwiper();
 
     return (
-        <div className='swiper-btn-click w-[100px] absolute right-[3%] bottom-[5%] bg-[rgba(255,255,255,0.2)] rounded-full z-[5] border-white border-[1px] h-[48px] overflow-hidden'>
+        <div className='swiper-btn-click w-[100px] h-[48px] max-2xl:h-[45px] absolute right-[3%] bottom-[5%] max-2xl:bottom-[4%] bg-[rgba(255,255,255,0.2)] rounded-full z-[5] border-white border-[1px] overflow-hidden'>
             <div className={`${currentIndex === 0 ? 'opacity-50 cursor-default' : 'opacity-100'} swiper-button-prev`} onClick={() => swiper.slidePrev()}></div>
             <div className={`${currentIndex === slideContent.length - 1 ? 'opacity-50 cursor-default' : 'opacity-100'} swiper-button-next`} onClick={() => swiper.slideNext()}></div>
         </div>   
