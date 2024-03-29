9be5421dcb6fbb680723a5e4ade24a6a91b4c960
Nattapat_BookZ <nattapatbook2078@gmail.com>
2024-03-30 01:09:07 +0700

    update

---
+++ src/components/Popular/popular-section.js
@@ -0,0 +1,38 @@
+import img1 from '../../assets/images/thumb_popular01.jpg';
+import img2 from '../../assets/images/thumb_popular02.jpg';
+import img3 from '../../assets/images/thumb_popular03.jpg';
+import img4 from '../../assets/images/thumb_popular04.jpg';
+import viewbox from '../../assets/images/view-label.svg';
+
+const popularContent = [
+    {
+        textContent: "HoYoverse กำลังพิจารณาว่าอาจจะนำอีเวนต์แบบจำกัดเวลาของ Genshin Impact มาให้เล่นย้อนหลังได้",
+        img: img1,
+        views: "1.5K",
+        ref: "KKMTC",
+        boxImg: viewbox
+    },
+    {
+        textContent: "7 เกมสุดย่ำแย่ เล่นแล้วท้อแท้กับชีวิต",
+        img: img2,
+        views: "1.5K",
+        ref: "KKMTC",
+        boxImg: viewbox
+    },
+    {
+        textContent: "6 เกม Open World แผนที่เล็ก แต่เต็มไปด้วย ชีวิตชีวา",
+        img: img3,
+        views: "1.5K",
+        ref: "KKMTC",
+        boxImg: viewbox
+    },
+    {
+        textContent: "แนะนำ Trombone Champ เกมดนตรีฮา ๆ แสดงจิตวิญญาณผ่านเสียงทรอมโบนสุดปั่น",
+        img: img4,
+        views: "1.5K",
+        ref: "KKMTC",
+        boxImg: viewbox
+    }
+];
+
+export default popularContent
