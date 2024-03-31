199bc6bc36c926b4c81422856bd455b21ccc9d92
Nattapat_BookZ <nattapatbook2078@gmail.com>
2024-04-01 01:05:22 +0700

    update

--- src/components/Popular/PopularSection.jsx
+++ src/components/Popular/PopularSection.jsx
@@ -17,23 +17,29 @@ const PopularSection = () => {
                 <div className='mt-8'>
                     <Swiper
                         slidesPerView={'auto'}
-                        spaceBetween={5}
+                        spaceBetween={40}
                         modules={[FreeMode]}
                         freeMode={true}
+                        breakpoints={{
+                            992: {
+                                slidesPerView: 4,
+                                spaceBetween: 64,
+                            }
+                        }}
                         className='mySwiper w-auto h-full'
                     >
                         {popularContent.map((data, index) => (
-                            <SwiperSlide key={index} className='w-auto overflow-hidden'>
-                                <div className='bg-red-500 w-fit'>
-                                    <div className='w-[250px] h-[150px]'>
-                                        <img src={data.img} alt="" />
+                            <SwiperSlide key={index} className='max-w-[250px] min-w-[150px] w-fulloverflow-hidden'>
+                                <div className='bg-red-500'>
+                                    <div>
+                                        <img className='w-full h-full object-cover' src={data.img} alt="" />
                                     </div>
-                                    <div className='flex'>
-                                        <div className='basis-[60%]'>
-                                            <p className='line-clamp-3'>{data.textContent}</p>
+                                    <div className='flex mt-4'>
+                                        <div className='basis-[75%] h-full'>
+                                            <h2 className='line-clamp-3 text-md'>{data.textContent}</h2>
                                         </div>
-                                        <div className='basis-[40%]'>
-                                            <p>{index + 1}</p>    
+                                        <div className='basis-[25%] text-center text-[80px] font-semibold h-full bg-green-500 relative -top-1'>
+                                            {index + 1}
                                         </div>
                                     </div>    
                                 </div> 
