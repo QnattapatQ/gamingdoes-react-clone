580da255cd0385e626631a99fd6761e0ea79479e
Nattapat_BookZ <nattapatbook2078@gmail.com>
2024-04-09 23:39:52 +0700

    update

---
+++ src/components/News/NewsList.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const NewsList = () => {
+    return (
+        <div>
+            <div>
+                <div className='flex justify-start items-start relative gap-6 w-full max-w-[960px] pb-6 mb-6'>
+                    <div className='basis-auto'>
+                        <a href="#">
+                            <img src="https://spicy-dev.com/preview/1dd/gamingdose/assets/images/thumb_news01.jpg" alt="" />
+                        </a>
+                    </div>
+                    <div>
+                        <a href="#">
+                            <h2 className='text-xl font-semibold hover:text-main-blue'>Overwatch 2 เตรียมเปิดให้เล่นแบบ Free-to-play ในวันที่ 5 ตุลาคมนี้ เวลา 02:00 น.</h2>
+                        </a>
+                        <div className='flex items-center justify-start gap-4 mt-4'>
+                            <div className='text-xs text-main-blue'>
+                                BY <a href="#">KMUTT</a>
+                            </div>
+                            <div className='text-xs text-main-blue'>
+                                <a href="#">NEWS</a>
+                            </div>
+                            <div>
+                                1 MINUTE AGO
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NewsList
--- src/components/News/NewsSection.jsx
+++ src/components/News/NewsSection.jsx
@@ -1,26 +1,27 @@
 import React from 'react'
+import NewsList from './NewsList'
 
 const NewsSection = () => {
     return (
         <div className='bg-white border-b-2'>
             <div className='max-w-[1332px] mx-auto'>
-                <div className='grid grid-cols-[1fr_332px]'>
+                <div className='grid grid-cols-[1fr_332px] px-[70px] max-2xl:px-10'>
                     <div className='border-r-2 py-16'>
                         <div className='flex items-start justify-stretch'>
-                            <div className='w-[75%] basis-auto pr-8'>
-                                <div className='px-[70px] max-2xl:px-10'>
+                            <div className='w-[75%] basis-auto mr-8 bg-green-500'>
+                                <div className=''>
                                     <h2 className='font-bold text-transparent uppercase text-4xl bg-main-blue-linear bg-clip-text w-fit'>NEWS</h2>
                                 </div>  
                                 <div className='my-12'>
-                                    asd
+                                    <NewsList/>
                                 </div>
                             </div>
-                            <div className='w-[25%] basis-auto'>
+                            <div className='w-[25%] basis-auto bg-green-400'>
                                 asdasd
                             </div>
                         </div>   
                     </div>
-                    <div className='py-16'>
+                    <div className='py-16 bg-red-300'>
 
                     </div>
                 </div>
