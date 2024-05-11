4d61f5c0d93d3e4741daa8d87ca86479ae7a82c7
Nattapat_BookZe <nattapatbook2078@gmail.com>
2024-05-11 12:49:31 +0700

    update

--- src/components/ButtonTransition/ButtonTransition.jsx
+++ src/components/ButtonTransition/ButtonTransition.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { FaChevronRight } from "react-icons/fa";
 
-const ButtonTransition = ({ textBtn }) => {
+const ButtonTransition = ({ textBtn, textColor }) => {
     return (
         <a href='#' className='uppercase group/chevron relative flex items-center justify-between'>
             <div className='mr-10 pl-3 cursor-pointer z-10'>
-                <h2 className='font-bold group-hover/chevron:text-white duration-200 uppercase'>{textBtn}</h2>
+                <h2 className={`${textColor ? 'text-white' : ''} font-bold group-hover/chevron:text-white duration-200 uppercase`}>{textBtn}</h2>
             </div>
             <div className='group-hover/chevron:w-full h-[32px] w-[32px] flex items-center justify-end pr-[7px] cursor-pointer bg-main-blue rounded-full absolute right-0 duration-200 '>
                 <FaChevronRight className='text-white'/>
--- src/components/WatchSection/WatchSection.jsx
+++ src/components/WatchSection/WatchSection.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import ButtonTransition from '../ButtonTransition/ButtonTransition';
 
 const WatchSection = () => {
     return (
         <div className='bg-red-500 h-[500px]'>
-
+            <div className='max-w-[1332px] mx-auto px-4'>
+                <div className='px-[70px]'>
+                    <div className='pt-16 pb-8'>
+                        <div className='flex max-lg:pr-0 w-full justify-between items-center'>
+                            <h2 className='font-bold text-transparent uppercase text-4xl text-white'>Watch</h2>
+                            <ButtonTransition textBtn={'Watch More'} textColor={true}/>
+                        </div>
+                    </div>  
+                </div>               
+            </div>
         </div>
     )
 }
